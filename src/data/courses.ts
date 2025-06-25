3292b26ee09f4cc251b9a0528834e381574c04c1
eeeeej <ej.cyclones2@gmail.com>
2025-06-25 08:59:37 -0500

    added evcc 18 hole rotations

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -5,26 +5,77 @@ export const courses: Course[] = [
     id: 4,
     name: "EVCC Vale/Ridge",
     holes: [
-      { number: 1, par: 4, yards: 380, handicap: 16 },
-      { number: 2, par: 4, yards: 502, handicap: 6 },
-      { number: 3, par: 4, yards: 390, handicap: 8 },
-      { number: 4, par: 4, yards: 331, handicap: 14 },
-      { number: 5, par: 3, yards: 188, handicap: 18 },
-      { number: 6, par: 5, yards: 516, handicap: 6 },
-      { number: 7, par: 3, yards: 106, handicap: 10 },
-      { number: 8, par: 4, yards: 418, handicap: 2 },
-      { number: 9, par: 4, yards: 464, handicap: 12 },
-      { number: 10, par: 4, yards: 446, handicap: 3 },
-      { number: 11, par: 4, yards: 384, handicap: 11 },
-      { number: 12, par: 3, yards: 202, handicap: 17 },
-      { number: 13, par: 5, yards: 392, handicap: 1 },
-      { number: 14, par: 4, yards: 573, handicap: 5 },
-      { number: 15, par: 3, yards: 397, handicap: 9 },
-      { number: 16, par: 4, yards: 402, handicap: 16 },
-      { number: 17, par: 4, yards: 178, handicap: 7 },
-      { number: 18, par: 5, yards: 543, handicap: 13 }
+      { number: 1, par: 4, yards: 334, handicap: 16 },
+      { number: 2, par: 4, yards: 448, handicap: 6 },
+      { number: 3, par: 4, yards: 345, handicap: 8 },
+      { number: 4, par: 4, yards: 357, handicap: 14 },
+      { number: 5, par: 3, yards: 148, handicap: 18 },
+      { number: 6, par: 5, yards: 552, handicap: 4 },
+      { number: 7, par: 3, yards: 190, handicap: 10 },
+      { number: 8, par: 4, yards: 434, handicap: 2 },
+      { number: 9, par: 4, yards: 337, handicap: 12 },
+      { number: 10, par: 4, yards: 362, handicap: 3 },
+      { number: 11, par: 4, yards: 393, handicap: 11 },
+      { number: 12, par: 3, yards: 157, handicap: 17 },
+      { number: 13, par: 5, yards: 538, handicap: 1 },
+      { number: 14, par: 4, yards: 429, handicap: 5 },
+      { number: 15, par: 3, yards: 188, handicap: 9 },
+      { number: 16, par: 4, yards: 361, handicap: 15 },
+      { number: 17, par: 4, yards: 365, handicap: 7 },
+      { number: 18, par: 5, yards: 524, handicap: 13 }
     ]
   },
+
+  {
+    id: 8,
+    name: "EVCC Vale/Creek",
+    holes: [
+      { number: 1, par: 4, yards: 334, handicap: 16 },
+      { number: 2, par: 4, yards: 448, handicap: 6 },
+      { number: 3, par: 4, yards: 345, handicap: 8 },
+      { number: 4, par: 4, yards: 357, handicap: 14 },
+      { number: 5, par: 3, yards: 148, handicap: 18 },
+      { number: 6, par: 5, yards: 552, handicap: 4 },
+      { number: 7, par: 3, yards: 190, handicap: 10 },
+      { number: 8, par: 4, yards: 434, handicap: 2 },
+      { number: 9, par: 4, yards: 337, handicap: 12 },
+      { number: 10, par: 4, yards: 332, handicap: 11 },
+      { number: 11, par: 5, yards: 517, handicap: 7 },
+      { number: 12, par: 3, yards: 169, handicap: 17 },
+      { number: 13, par: 4, yards: 302, handicap: 15 },
+      { number: 14, par: 4, yards: 372, handicap: 3 },
+      { number: 15, par: 5, yards: 490, handicap: 9 },
+      { number: 16, par: 4, yards: 440, handicap: 1 },
+      { number: 17, par: 3, yards: 177, handicap: 13 },
+      { number: 18, par: 4, yards: 407, handicap: 5 }
+    ]
+  },
+  
+  {
+    id: 5,
+    name: "EVCC - Creek/Ridge",
+    holes: [
+      { number: 1, par: 4, yards: 332, handicap: 11 },
+      { number: 2, par: 5, yards: 517, handicap: 7 },
+      { number: 3, par: 3, yards: 169, handicap: 17 },
+      { number: 4, par: 4, yards: 302, handicap: 15 },
+      { number: 5, par: 4, yards: 372, handicap: 3 },
+      { number: 6, par: 5, yards: 490, handicap: 9 },
+      { number: 7, par: 4, yards: 440, handicap: 1 },
+      { number: 8, par: 3, yards: 177, handicap: 13 },
+      { number: 9, par: 4, yards: 407, handicap: 5 },
+      { number: 10, par: 4, yards: 362, handicap: 4 },
+      { number: 11, par: 4, yards: 393, handicap: 12 },
+      { number: 12, par: 3, yards: 157, handicap: 18 },
+      { number: 13, par: 5, yards: 538, handicap: 2 },
+      { number: 14, par: 4, yards: 429, handicap: 6 },
+      { number: 15, par: 3, yards: 188, handicap: 10 },
+      { number: 16, par: 4, yards: 361, handicap: 16 },
+      { number: 17, par: 4, yards: 365, handicap: 8 },
+      { number: 18, par: 5, yards: 524, handicap: 14 }
+    ]
+  },
+
   {
     id: 1,
     name: "EVCC Vale",
@@ -55,36 +106,21 @@ export const courses: Course[] = [
       { number: 9, par: 4, yards: 407, handicap: 5 }
     ]
   },
-  
+
   {
-    id: 3,
-    name: "EVCC Ridge(odd)",
+    id: 6,
+    name: "EVCC Ridge",
     holes: [
-      { number: 10, par: 4, yards: 362, handicap: 3 },
-      { number: 11, par: 4, yards: 393, handicap: 11 },
-      { number: 12, par: 3, yards: 157, handicap: 17 },
-      { number: 13, par: 5, yards: 538, handicap: 1 },
-      { number: 14, par: 4, yards: 429, handicap: 5 },
-      { number: 15, par: 3, yards: 188, handicap: 9 },
-      { number: 16, par: 4, yards: 361, handicap: 15 },
-      { number: 17, par: 4, yards: 365, handicap: 7 },
-      { number: 18, par: 5, yards: 524, handicap: 13 }
-    ]
-  },
-  
-  {
-    id: 5,
-    name: "EVCC - Ridge(even)",
-    holes: [
-      { number: 10, par: 4, yards: 362, handicap: 4 },
-      { number: 11, par: 4, yards: 393, handicap: 12 },
-      { number: 12, par: 3, yards: 157, handicap: 18 },
-      { number: 13, par: 5, yards: 538, handicap: 2 },
-      { number: 14, par: 4, yards: 429, handicap: 6 },
-      { number: 15, par: 3, yards: 188, handicap: 10 },
-      { number: 16, par: 4, yards: 361, handicap: 16 },
-      { number: 17, par: 4, yards: 365, handicap: 8 },
-      { number: 18, par: 5, yards: 524, handicap: 14 }
+      { number: 1, par: 4, yards: 362, handicap: 3 },
+      { number: 2, par: 4, yards: 393, handicap: 11 },
+      { number: 3, par: 3, yards: 157, handicap: 17 },
+      { number: 4, par: 5, yards: 538, handicap: 1 },
+      { number: 5, par: 4, yards: 429, handicap: 5 },
+      { number: 6, par: 3, yards: 188, handicap: 9 },
+      { number: 7, par: 4, yards: 361, handicap: 15 },
+      { number: 8, par: 4, yards: 365, handicap: 7 },
+      { number: 9, par: 5, yards: 524, handicap: 13 }
     ]
   }
+
 ];
